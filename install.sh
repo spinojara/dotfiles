ccfdc96317a6c99695a77a3f3e8773e4fe21cb61
Isak Ellmer <isak01@gmail.com>
2025-05-10 07:57:13 +0200

    Add hyprland and waybar config

--- install.sh
+++ install.sh
@@ -23,3 +23,11 @@ cp vim/tex.vim $HOME/.vim/ftplugin
 
 # tmux
 cp tmux/tmux.conf $HOME/.tmux.conf
+
+# hyprland
+mkdir -p $HOME/.config/hypr
+cp hypr/{gruvbox,hypridle,hyprland,hyprlock}.conf $HOME/.config/hypr
+
+# waybar
+mkdir -p $HOME/.config/waybar
+cp waybar/{config.jsonc,style.css} $HOME/.config/waybar
