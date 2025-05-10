fb67e96bd445528c3096b040070fb62dd97bd026
Isak Ellmer <isak01@gmail.com>
2025-05-10 07:44:26 +0200

    Merge install scripts

--- install.sh
+++ install.sh
@@ -11,3 +11,15 @@ cp bash/bash_profile $HOME/.bash_profile
 
 # git
 cp git/gitconfig $HOME/.gitconfig
+
+# bash
+cp bash/bashrc $HOME/.bashrc
+
+# vim
+mkdir -p $HOME/.vim/{ftplugin,after/syntax}
+cp vim/vimrc $HOME/.vim
+cp vim/{syncolor,c}.vim $HOME/.vim/after/syntax
+cp vim/tex.vim $HOME/.vim/ftplugin
+
+# tmux
+cp tmux/tmux.conf $HOME/.tmux.conf
