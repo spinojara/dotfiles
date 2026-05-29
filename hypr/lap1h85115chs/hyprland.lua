17c4f3ee3287c9b7bdbc2d1bdb6d5ade6c1b8a4b
Isak Ellmer <isak01@gmail.com>
2026-05-29 16:48:51 +0200

    Fix hyprland.lua

--- hypr/lap1h85115chs/hyprland.lua
+++ hypr/lap1h85115chs/hyprland.lua
@@ -10,13 +10,16 @@ hl.monitor({
 })
 
 add_workspaces({
-	["1"] = m1,
-	["2"] = m1,
-	["3"] = m1,
-	["4"] = m1,
-	["5"] = m1,
-	["6"] = m1,
-	["7"] = m1,
-	["8"] = m1,
-	["9"] = m1
+	{"1", m1, true},
+	{"2", m1, false},
+	{"3", m1, false},
+	{"4", m1, false},
+	{"5", m1, false},
+	{"6", m1, false},
+	{"7", m1, false},
+	{"8", m1, false},
+	{"9", m1, false}
 })
+
+hl.bind("ALT + SHIFT + p", hl.dsp.exec_cmd("systemctl poweroff"), { long_press = true })
+hl.bind("ALT + SHIFT + o", hl.dsp.exec_cmd("systemctl reboot"), { long_press = true })
